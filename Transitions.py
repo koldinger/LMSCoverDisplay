97f68ffb16bcb6a390dbcec7c48bfb4155cb2026
Eric Koldinger <kolding@washington.edu>
2025-10-23 14:46:26 -0700

    Added more transitions.   More to come.   Probably.

--- Transitions.py
+++ Transitions.py
@@ -1,14 +1,14 @@
-from PIL import Image
-import time
-import rich.traceback
-rich.traceback.install()
-
 import random
-
-from icecream import ic 
-ic.configureOutput(includeContext=True)
-
+import time
 from enum import StrEnum, auto
+
+from PIL import Image, ImageEnhance
+
+#import rich.traceback
+#rich.traceback.install()
+#from icecream import ic 
+#ic.configureOutput(includeContext=True)
+
 
 class TransitionTypes(StrEnum):
     none = auto()
@@ -20,27 +20,44 @@ class TransitionTypes(StrEnum):
     OverDown = auto()
     OverLeft = auto()
     OverRight = auto()
+    WipeLeft = auto()
+    WipeRight = auto()
+    WipeUp = auto()
+    WipeDown = auto()
     DownUp = auto()
+    SlideRightDown = auto()
+    SlideRightUp = auto()
+    SlideLeftDown = auto()
+    SlideLeftUp = auto()
+    ExpandRightDown = auto()
+    ExpandRightUp = auto()
+    ExpandLeftDown = auto()
+    ExpandLeftUp = auto()
+    CurtainHoriz = auto()
+    CurtainVert = auto()
+    CurtainOutHoriz = auto()
+    CurtainOutVert = auto()
+    ZoomIn = auto()
+    ZoomOut = auto()
+    Fade = auto()
+    FadeOutIn = auto()
+    Transporter = auto()
     Random = auto()
 
 
-
 def fade(oimg, nimg, steps):
-    ic()
-    oimg =  oimg.convert("RGBA")
-    nimg =  oimg.convert("RGBA")
-
     for i in range(steps + 1):
       alpha = float(i / steps)  # Incremental alpha values
       blended = Image.blend(oimg, nimg, alpha).convert("RGB")
-      ic(alpha, blended)
       yield blended
       
-def doPush(oimg, nimg, steps):
-    ic()
+def _rotate(oimg, nimg, steps, angle, func):
+    oimg = oimg.rotate(angle)
+    nimg = nimg.rotate(angle)
+    return map(lambda x: x.rotate(-angle), func(oimg, nimg, steps))
 
+def _doPush(oimg, nimg, steps):
     inc = oimg.width / steps
-    ic(inc)
 
     width, height = oimg.size
 
@@ -59,72 +76,216 @@ def doPush(oimg, nimg, steps):
                 respx[x, y] = cpx[x + shift, y]
         yield res
 
-def pushLeft(oimg, nimg, steps):
-    return doPush(oimg, nimg, steps)
-
-def pushRight(oimg, nimg, steps):
-    angle = 180
-    oimg = oimg.rotate(angle)
-    nimg = nimg.rotate(angle)
-
-    return map(lambda x: x.rotate(-angle), doPush(oimg, nimg, steps))
-
-def pushUp(oimg, nimg, steps):
-    angle = 90
-    oimg = oimg.rotate(angle)
-    nimg = nimg.rotate(angle)
-
-    return map(lambda x: x.rotate(-angle), doPush(oimg, nimg, steps))
-
-
-def pushDown(oimg, nimg, steps):
-    angle = -90
-    oimg = oimg.rotate(angle)
-    nimg = nimg.rotate(angle)
-
-    return map(lambda x: x.rotate(-angle), doPush(oimg, nimg, steps))
-
-def doOver(oimg, nimg, steps):
+def _doOver(oimg, nimg, steps):
     width, height = oimg.size
     inc = int(width / steps)
     for i in range(steps, -1, -1):
         shift = inc * i
         img = oimg.copy()
-        ic(shift, width)
 
         over = nimg.crop((shift, 0, width, height))
         img.paste(over, (0, 0))
         yield img
 
+def _doSlide(oimg, nimg, steps):
+    width, height = nimg.size
+    for i in range(1, steps + 1):
+        amt = int(i * width / steps)
+        img = oimg.copy()
+        over = nimg.crop((width - amt, height - amt, width, height))
+        img.paste(over, (0, 0))
+        yield img
+
+def _doWipe(oimg, nimg, steps):
+    width, height = nimg.size
+
+    for i in range(1, steps + 1):
+        amt = int(i * width / steps)
+        img = oimg.copy()
+        over = nimg.crop((0, 0, amt, height))
+        img.paste(over, (0, 0))
+        yield img
+
+def _doCurtain(oimg, nimg, steps):
+    left = nimg.crop((0, 0, int(nimg.width / 2), nimg.height))
+    right = nimg.crop((int(nimg.width / 2), 0, nimg.width, nimg.height))
+
+    increment = left.width / steps
+
+    for i in range(steps + 1):
+        img = oimg.copy()
+        width = int(increment * i)
+        lc = left.crop((left.width - width, 0, left.width, left.height))
+        img.paste(lc, (0, 0))
+
+        rc = right.crop((right.width - width, 0, right.width, right.height))
+        img.paste(rc, (img.width-width, 0))
+
+        yield img
+
+def _doCurtainOut(oimg, nimg, steps):
+    left = oimg.crop((0, 0, int(nimg.width / 2), nimg.height))
+    right = oimg.crop((int(nimg.width / 2), 0, nimg.width, nimg.height))
+
+    increment = left.width / steps
+
+    for i in range(steps, -1, -1):
+        img = nimg.copy()
+        width = int(increment * i)
+        lc = left.crop((left.width - width, 0, left.width, left.height))
+        img.paste(lc, (0, 0))
+
+        rc = right.crop((right.width - width, 0, right.width, right.height))
+        img.paste(rc, (img.width-width, 0))
+
+        yield img
+
+def pushLeft(oimg, nimg, steps):
+    return _doPush(oimg, nimg, steps)
+
+def pushRight(oimg, nimg, steps):
+    return _rotate(oimg, nimg, steps, 180, _doPush)
+
+def pushUp(oimg, nimg, steps):
+    return _rotate(oimg, nimg, steps, 90, _doPush)
+
+def pushDown(oimg, nimg, steps):
+    return _rotate(oimg, nimg, steps, -90, _doPush)
+
 def overRight(oimg, nimg, steps):
-    return doOver(oimg, nimg, steps)
+    return _doOver(oimg, nimg, steps)
 
 def overLeft(oimg, nimg, steps):
-    angle = 180
-    oimg = oimg.rotate(angle)
-    nimg = nimg.rotate(angle)
-
-    return map(lambda x: x.rotate(-angle), doOver(oimg, nimg, steps))
+    return _rotate(oimg, nimg, steps, 180, _doOver)
 
 def overUp(oimg, nimg, steps):
-    angle = -90
-    oimg = oimg.rotate(angle)
-    nimg = nimg.rotate(angle)
-
-    return map(lambda x: x.rotate(-angle), doOver(oimg, nimg, steps))
+    return _rotate(oimg, nimg, steps, -90, _doOver)
 
 def overDown(oimg, nimg, steps):
-    angle = 90
-    oimg = oimg.rotate(angle)
-    nimg = nimg.rotate(angle)
+    return _rotate(oimg, nimg, steps, 90, _doOver)
 
-    return map(lambda x: x.rotate(-angle), doOver(oimg, nimg, steps))
+def wipeRight(oimg, nimg, steps):
+    return _doWipe(oimg, nimg, steps)
+
+def wipeLeft(oimg, nimg, steps):
+    return _rotate(oimg, nimg, steps, 180, _doWipe)
+
+def wipeUp(oimg, nimg, steps):
+    return _rotate(oimg, nimg, steps, -90, _doWipe)
+
+def wipeDown(oimg, nimg, steps):
+    return _rotate(oimg, nimg, steps, 90, _doWipe)
+
+def slideRightDown(oimg, nimg, steps):
+    return _doSlide(oimg, nimg, steps)
+
+def slideRightUp(oimg, nimg, steps):
+    return _rotate(oimg, nimg, steps, -90, _doSlide)
+
+def slideLeftDown(oimg, nimg, steps):
+    return _rotate(oimg, nimg, steps, 90, _doSlide)
+
+def slideLeftUp(oimg, nimg, steps):
+    return _rotate(oimg, nimg, steps, 180, _doSlide)
+
+def _doExpand(oimg, nimg, steps):
+    width, height = nimg.size
+    for i in range(1, steps + 1):
+        size = int(i * width / steps)
+        img = oimg.copy()
+        over = nimg.resize((size, size))
+        img.paste(over, (0, 0))
+        yield img
+
+def expandRightDown(oimg, nimg, steps):
+    return _doExpand(oimg, nimg, steps)
+
+def expandRightUp(oimg, nimg, steps):
+    return _rotate(oimg, nimg, steps, -90, _doExpand)
+
+def expandLeftDown(oimg, nimg, steps):
+    return _rotate(oimg, nimg, steps, 90, _doExpand)
+
+def expandLeftUp(oimg, nimg, steps):
+    return _rotate(oimg, nimg, steps, 180, _doExpand)
+
+def zoomIn(oimg, nimg, steps):
+    w = oimg.width / steps
+    h = oimg.height / steps
+
+    cw = nimg.width / 2
+    ch = nimg.height / 2
+
+    yield oimg
+
+    for i in range(1, steps + 1):
+        img = oimg.copy()
+        cpos = (int(cw - (w * i / 2)), int(ch - (h * i / 2)))
+        csize = ((int(nimg.width / steps * i), int(nimg.height / steps * i)))
+        cimg = nimg.resize((int(nimg.width / steps * i), int(nimg.height / steps * i)))
+        img.paste(cimg, cpos)
+        yield img
+
+def zoomOut(oimg, nimg, steps):
+    w = oimg.width / steps
+    h = oimg.height / steps
+
+    cw = nimg.width / 2
+    ch = nimg.height / 2
+
+    yield oimg
+
+    for i in range(steps, 0, -1):
+        img = nimg.copy()
+        cpos = (int(cw - (w * i / 2)), int(ch - (h * i / 2)))
+        csize = ((int(nimg.width / steps * i), int(nimg.height / steps * i)))
+        cimg = oimg.resize((int(nimg.width / steps * i), int(nimg.height / steps * i)))
+        img.paste(cimg, cpos)
+        yield img
+
+    yield nimg
 
 def downUp(oimg, nimg, steps):
     blank = Image.new("RGB", oimg.size)
     yield from pushDown(oimg, blank, int(steps / 2))
     yield from pushUp(blank, nimg, int(steps / 2))
 
+
+def curtainHoriz(oimg, nimg, steps):
+    return _doCurtain(oimg, nimg, steps)
+
+def curtainVert(oimg, nimg, steps):
+    return _rotate(oimg, nimg, steps, 90, _doCurtain)
+
+def curtainOutHoriz(oimg, nimg, steps):
+    return _doCurtainOut(oimg, nimg, steps)
+
+def curtainOutVert(oimg, nimg, steps):
+    return _rotate(oimg, nimg, steps, 90, _doCurtainOut)
+
+def fadeOutIn(oimg, nimg, steps):
+    steps = int(steps / 2)
+
+    for i in range(steps + 1, 0, -1):
+        enhancer = ImageEnhance.Brightness(oimg)
+        enchanced = enhancer.enhance(float(i / steps))
+        yield enchanced
+    for i in range(steps + 1):
+        enhancer = ImageEnhance.Brightness(nimg)
+        enchanced = enhancer.enhance(float(i / steps))
+        yield enchanced
+
+def transporter(oimg, nimg, steps):
+    px = nimg.load()
+    for i in range(steps + 1):
+        threshold = i / steps
+        img = oimg.copy()
+        ipx = oimg.load()
+        for x in range(oimg.width):
+            for y in range(oimg.height):
+                if random.random() < threshold:
+                    ipx[x, y] = px[x, y]
+        yield img
 
 def noTransition(oimg, nimg, steps):
     yield nimg
@@ -133,8 +294,6 @@ choices = [x for x in TransitionTypes][:-1]
 
 def getTransition(transition: TransitionTypes):
     match transition:
-        case TransitionTypes.none:
-            return noTransition
         case TransitionTypes.PushUp:
             return pushUp
         case TransitionTypes.PushDown:
@@ -151,25 +310,72 @@ def getTransition(transition: TransitionTypes):
             return overLeft
         case TransitionTypes.OverRight:
             return overRight
+        case TransitionTypes.WipeUp:
+            return wipeUp
+        case TransitionTypes.WipeDown:
+            return wipeDown
+        case TransitionTypes.WipeLeft:
+            return wipeLeft
+        case TransitionTypes.WipeRight:
+            return wipeRight
         case TransitionTypes.DownUp:
             return downUp
+        case TransitionTypes.SlideRightDown:
+            return slideRightDown
+        case TransitionTypes.SlideRightUp:
+            return slideRightUp
+        case TransitionTypes.SlideLeftDown:
+            return slideLeftDown
+        case TransitionTypes.SlideLeftUp:
+            return slideLeftUp
+        case TransitionTypes.ExpandRightDown:
+            return expandRightDown
+        case TransitionTypes.ExpandRightUp:
+            return expandRightUp
+        case TransitionTypes.ExpandLeftDown:
+            return expandLeftDown
+        case TransitionTypes.ExpandLeftUp:
+            return expandLeftUp
+        case TransitionTypes.CurtainHoriz:
+            return curtainHoriz
+        case TransitionTypes.CurtainVert:
+            return curtainVert
+        case TransitionTypes.CurtainOutHoriz:
+            return curtainOutHoriz
+        case TransitionTypes.CurtainOutVert:
+            return curtainOutVert
+        case TransitionTypes.ZoomIn:
+            return zoomIn
+        case TransitionTypes.ZoomOut:
+            return zoomOut
+        case TransitionTypes.Fade:
+            return fade
+        case TransitionTypes.FadeOutIn:
+            return fadeOutIn
+        case TransitionTypes.Transporter:
+            return transporter
         case TransitionTypes.Random:
             return getTransition(random.choice(choices))
+        case TransitionTypes.none:
+            return noTransition
+        case _:
+            raise Exception(f"Unknown transition: {transition}")
 
 
 if __name__ == "__main__":
-    ic()
     import flaschen
-    f = flaschen.Flaschen("raspberrypi.local", 1337, 64, 64)
+    import sys
+    size = 128
 
-    img2 = Image.open("726420.ppm")
-    img1 = Image.open("750783.ppm")
+    f = flaschen.Flaschen("localhost", 1337, size, size)
+
+    img1 = Image.open("cover1.jpg").resize((size, size))
+    img2 = Image.open("cover2.jpg").resize((size, size))
 
     def sendArt(f, i):
-        ic()
         px = i.load()
-        for x in range(64):
-            for y in range(64):
+        for x in range(size):
+            for y in range(size):
                 f.set(x, y, px[x, y])
         f.send()
 
@@ -178,11 +384,20 @@ if __name__ == "__main__":
             sendArt(f, i)
             time.sleep(pause)
 
-    for i in TransitionTypes:
+    # doTransition(f, expandRightDown(img1, img2, 10))
+    # time.sleep(3)
+    if len(sys.argv) > 1:
+        transitions = sys.argv[1:]
+    else:
+        transitions = TransitionTypes
+
+    for i in transitions:
         print(i)
+        sendArt(f, img1)
+        time.sleep(0.5)
         trans = getTransition(i)
-        doTransition(f, trans(img1, img2, 10))
+        doTransition(f, trans(img1, img2, 20))
         time.sleep(2)
 
-
+    sendArt(f, Image.new("RGB", img1.size))
 
