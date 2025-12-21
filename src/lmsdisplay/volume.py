d1b430c376bd4ae5ce9231d29d798437dd20d6ca
Eric Koldinger <kolding@washington.edu>
2025-12-20 21:08:14 -0800

    Made the position and size of the volume bar adjustable.

--- src/lmsdisplay/volume.py
+++ src/lmsdisplay/volume.py
@@ -1,16 +1,16 @@
 from PIL import Image, ImageDraw
 
-def drawVolume(volume, size=(500, 500), color=(255, 255, 255, 255)):
+def drawVolume(volume, size=(500, 500), color=(255, 255, 255, 255), xoffset=.05, yoffset=.85, yheight=0.1):
     x, y = size
 
     canvas = Image.new("RGBA", size, color=(0, 0, 0, 0))
     draw = ImageDraw.Draw(canvas)
 
-    top_x = int(.05 * float(x))
-    bot_x = int(.95 * float(x))
+    top_x = int(xoffset * float(x))
+    bot_x = int((1.0 - xoffset) * float(x))
 
-    top_y = int(.85 * float(y))
-    bot_y = int(.95 * float(y))
+    top_y = int(yoffset * float(y))
+    bot_y = int((yoffset + yheight) * float(y))
 
     draw.rounded_rectangle([(top_x, top_y), (bot_x, bot_y)], radius=5, outline=color, width=1)
 
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     background = background.resize(size)
 
     for volume in range(0, 101, 10):
-        vol = drawVolume(volume, size, (211, 211, 211, 200))
+        vol = drawVolume(volume, size, (211, 100, 100, 200), xoffset=.05, yoffset=.9, yheight=.025)
         img = background.copy()
         img.paste(vol, (0, 0), vol)
         img.convert("RGB").save(f"test{volume}.jpg")
