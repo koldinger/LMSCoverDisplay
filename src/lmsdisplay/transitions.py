456ccd369b2262569702c87390b6b2c54a73559c
Eric Koldinger <kolding@washington.edu>
2026-01-14 08:20:20 -0800

    Cleaned up the way some transitions are handled, so that we don't need to have functions for each version of transitions that can be rotated.

--- src/lmsdisplay/transitions.py
+++ src/lmsdisplay/transitions.py
@@ -1,14 +1,14 @@
+import itertools
 import random
 import time
-from enum import StrEnum, auto
 from contextlib import suppress
+from enum import StrEnum, auto
+from functools import partial
 
-from PIL import Image, ImageEnhance
 import numpy as np
-
-from numpy.linalg import LinAlgError
 import rich.traceback
-import itertools
+from numpy.linalg import LinAlgError
+from PIL import Image, ImageEnhance
 
 rich.traceback.install()
 
@@ -21,8 +21,8 @@ Helper functions.   Perform an action.   In many cases, the images can be rotate
 transition, and then rotated back to perform the operation in a certain direction.
 """
 
-def _rotate(angle: int, func, oimg: Image.Image, nimg: Image, steps: int, *moreargs):
-    """Rotate 2 images by 'angle' degrees, perform the transition, and rotate the result back."""
+def _rotate(angle: int, func, oimg: Image.Image, nimg: Image.Image, steps: int, *moreargs):
+    """ Rotate 2 images by 'angle' degrees, perform the transition, and rotate the result back. """
     r_oimg = oimg.rotate(angle)
     r_nimg = nimg.rotate(angle)
     #return map(lambda x: x.rotate(-angle), func(oimg, nimg, steps, *moreargs))
@@ -94,7 +94,7 @@ def _doSlide(oimg, nimg, steps):
     yield nimg
 
 
-def _doCurtain(oimg, nimg, steps):
+def _doCurtain(oimg, nimg, steps, out=False):
     """Perform a curtain, closing in with the new image from both sides."""
     left = nimg.crop((0, 0, int(nimg.width / 2), nimg.height))
     right = nimg.crop((int(nimg.width / 2), 0, nimg.width, nimg.height))
@@ -244,154 +244,43 @@ Actual functions that do the work.   They'll either call the helper function dir
 to make it go in a different direction
 """
 
-def pushLeft(oimg, nimg, steps):
-    return _doPush(oimg, nimg, steps)
+def push(oimg, nimg, steps, rotation = 0):
+    return _rotate(rotation ,_doPush, oimg, nimg, steps)
 
 
-def pushRight(oimg, nimg, steps):
-    return _rotate(180, _doPush, oimg, nimg, steps)
-
-
-def pushUp(oimg, nimg, steps):
-    return _rotate(90, _doPush, oimg, nimg, steps)
-
-
-def pushDown(oimg, nimg, steps):
-    return _rotate(270, _doPush, oimg, nimg, steps)
-
-
-def overRight(oimg, nimg, steps):
-    yield from _doOver(oimg, nimg, steps, True)
+def over(oimg, nimg, steps, rotation = 0):
+    yield from _rotate(rotation, _doOver, oimg, nimg, steps, True)
     yield nimg
 
 
-def overLeft(oimg, nimg, steps):
-    yield from _rotate(180, _doOver, oimg, nimg, steps, True)
+def unCover(oimg, nimg, steps, rotation = 0):
+    yield from _rotate(rotation, _doOver, nimg, oimg, steps, False)
     yield nimg
 
 
-def overUp(oimg, nimg, steps):
-    yield from _rotate(270, _doOver, oimg, nimg, steps, True)
+def wipe(oimg, nimg, steps, rotation = 0):
+    return _rotate(rotation, _doWipe, oimg, nimg, steps)
+
+
+def slide(oimg, nimg, steps, rotation=0):
+    return _rotate(rotation, _doSlide, oimg, nimg, steps)
+
+def expand(oimg, nimg, steps, rotation=0):
+    yield from _rotate(rotation, _doExpand, oimg, nimg, steps, True)
     yield nimg
 
-
-def overDown(oimg, nimg, steps):
-    yield from _rotate(90, _doOver, oimg, nimg, steps, True)
+def shrink(oimg, nimg, steps, rotation=0):
+    yield from _rotate(rotation, _doExpand, nimg, oimg, steps, False)
     yield nimg
 
+def curtain(oimg, nimg, steps, rotation=0):
+    return _rotate(rotation, _doCurtain, oimg, nimg, steps)
 
-def unCoverLeft(oimg, nimg, steps):
-    yield from _doOver(nimg, oimg, steps, False)
-    yield nimg
-
-
-def unCoverRight(oimg, nimg, steps):
-    yield from _rotate(180, _doOver, nimg, oimg, steps, False)
-    yield nimg
-
-
-def unCoverUp(oimg, nimg, steps):
-    yield from _rotate(90, _doOver, nimg, oimg, steps, False)
-    yield nimg
-
-
-def unCoverDown(oimg, nimg, steps):
-    yield from _rotate(270, _doOver, nimg, oimg, steps, False)
-    yield nimg
-
-
-def wipeRight(oimg, nimg, steps):
-    return _doWipe(oimg, nimg, steps)
-
-
-def wipeLeft(oimg, nimg, steps):
-    return _rotate(180, _doWipe, oimg, nimg, steps)
-
-
-def wipeUp(oimg, nimg, steps):
-    return _rotate(270, _doWipe, oimg, nimg, steps)
-
-
-def wipeDown(oimg, nimg, steps):
-    return _rotate(90, _doWipe, oimg, nimg, steps)
-
-
-def slideRightDown(oimg, nimg, steps):
-    return _doSlide(oimg, nimg, steps)
-
-
-def slideRightUp(oimg, nimg, steps):
-    return _rotate(270, _doSlide, oimg, nimg, steps)
-
-
-def slideLeftDown(oimg, nimg, steps):
-    return _rotate(90, _doSlide, oimg, nimg, steps)
-
-
-def slideLeftUp(oimg, nimg, steps):
-    return _rotate(180, _doSlide, oimg, nimg, steps)
-
-
-def expandRightDown(oimg, nimg, steps):
-    yield from _doExpand(oimg, nimg, steps, True)
-    yield nimg
-
-
-def expandRightUp(oimg, nimg, steps):
-    yield from _rotate(270, _doExpand, oimg, nimg, steps, True)
-    yield nimg
-
-
-def expandLeftDown(oimg, nimg, steps):
-    yield from _rotate(90, _doExpand, oimg, nimg, steps, True)
-    yield nimg
-
-
-def expandLeftUp(oimg, nimg, steps):
-    yield from _rotate(180, _doExpand, oimg, nimg, steps, True)
-    yield nimg
-
-
-def shrinkLeftUp(oimg, nimg, steps):
-    yield from _doExpand(nimg, oimg, steps, False)
-    yield nimg
-
-
-def shrinkRightDown(oimg, nimg, steps):
-    yield from _rotate(180, _doExpand, nimg, oimg, steps, False)
-    yield nimg
-
-
-def shrinkRightUp(oimg, nimg, steps):
-    yield from _rotate(90, _doExpand, nimg, oimg, steps, False)
-    yield nimg
-
-
-def shrinkLeftDown(oimg, nimg, steps):
-    yield from _rotate(270, _doExpand, nimg, oimg, steps, False)
-    yield nimg
-
-
-
-def curtainHoriz(oimg, nimg, steps):
-    return _doCurtain(oimg, nimg, steps)
-
-
-def curtainVert(oimg, nimg, steps):
-    return _rotate(90, _doCurtain, oimg, nimg, steps)
-
-
-def curtainOutHoriz(oimg, nimg, steps):
-    return _doCurtainOut(oimg, nimg, steps)
-
-
-def curtainOutVert(oimg, nimg, steps):
-    return _rotate(90, _doCurtainOut, oimg, nimg, steps)
-
+def curtainOut(oimg, nimg, steps, rotation=0):
+    return _rotate(rotation, _doCurtain, oimg, nimg, steps)
 
 def spinOut(oimg, nimg, steps):
     return _doSpin(oimg, nimg, steps, True)
-
 
 def spinIn(oimg, nimg, steps):
     yield from _doSpin(nimg, oimg, steps, False)
@@ -419,8 +308,8 @@ def flipOut(oimg, nimg, steps):
 def downUp(oimg, nimg, steps):
     """Lower the current image, and then raise the new image up."""
     blank = Image.new("RGB", oimg.size)
-    yield from pushDown(oimg, blank, int(steps / 2))
-    yield from pushUp(blank, nimg, int(steps / 2))
+    yield from _rotate(270, push, oimg, blank, int(steps / 2))
+    yield from _rotate(90, push, blank, nimg, int(steps / 2))
 
 
 def zoomOutIn(oimg, nimg, steps):
@@ -524,38 +413,26 @@ def fadeOutIn(oimg, nimg, steps):
         yield enchanced
     yield nimg
 
-def shimmer(oimg, nimg, steps):
+def shimmer(oimg, nimg, steps, keep=False):
     """
     Change from one image to another by replacing pixels with pixels from the new image.
     Pixels can shift back and forth between the two images, with the percentage of new pixels
     increasing until all pixels have changed.
     """
     px = nimg.load()
+    img = oimg.copy()
     for i in range(steps + 1):
         threshold = i / steps
-        img = oimg.copy()
         ipx = img.load()
         for x in range(oimg.width):
             for y in range(oimg.height):
                 if random.random() < threshold:
                     ipx[x, y] = px[x, y]
         yield img
+        if not keep:
+            img = oimg.copy()
     yield nimg
 
-
-def transporter(oimg, nimg, steps):
-    """ Do a 'beam in' effect where pixels are gradually replaced with pixels from the new image. """
-    px = nimg.load()
-    img = oimg.copy()
-    ipx = img.load()
-    for i in range(steps + 1):
-        threshold = i / steps
-        for x in range(oimg.width):
-            for y in range(oimg.height):
-                if random.random() < threshold:
-                    ipx[x, y] = px[x, y]
-        yield img
-    yield nimg
 
 def _makeSquares(chunk, size, snake):
     squares = []
@@ -624,14 +501,9 @@ def _doBars(oimg, nimg, steps):
             y += chunk.height
         yield res
 
-def barshoriz(oimg, nimg, steps):
+def bars(oimg, nimg, steps, rotation=0):
     yield oimg
-    yield from _doBars(oimg, nimg, steps)
-    yield nimg
-
-def barsvertical(oimg, nimg, steps):
-    yield oimg
-    yield from _rotate(90, _doBars, oimg, nimg, steps)
+    yield from _rotate(rotation, _doBars, oimg, nimg, steps)
     yield nimg
 
 
@@ -654,39 +526,39 @@ class TransitionTypes(StrEnum):
         return member
 
     Instant = auto(), "Instant Transition", instant
-    PushLeft = auto(), "Push the old image out to the left", pushLeft
-    PushRight = auto(),"Push the old image out to the right", pushRight
-    PushUp = auto(), "Push the old image up from the bottom", pushUp
-    PushDown = auto(),"Push the old image down from the top", pushDown
-    OverUp = auto(),"Pull the new image up from the bottom", overUp
-    OverDown = auto(), "Pull the new image down from the top", overDown
-    OverLeft = auto(), "Pull the new image in from the right", overLeft
-    OverRight = auto(), "Pull the new image in from the left", overRight
-    UncoverLeft = auto(), "Pull the old image away to the left", unCoverLeft
-    UncoverRight = auto(), "Pull the old image away to the right", unCoverRight
-    UncoverUp = auto(), "Pull the old image away to the top", unCoverUp
-    UncoverDown = auto(), "Pull the old image away to the bottom", unCoverDown
-    WipeLeft = auto(), "Wipe left from the right side", wipeLeft
-    WipeRight = auto(), "Pull the new image in from the left", wipeRight
-    WipeUp = auto(), "Wipe up from the bottom", wipeUp
-    WipeDown = auto(), "Wipe down from the top", wipeDown
+    PushLeft = auto(), "Push the old image out to the left", partial(push, rotation=0)
+    PushRight = auto(),"Push the old image out to the right", partial(push, rotation=180)
+    PushUp = auto(), "Push the old image up from the bottom", partial(push, rotation=90)
+    PushDown = auto(),"Push the old image down from the top", partial(push, rotation=270)
+    OverUp = auto(),"Pull the new image up from the bottom", partial(over, rotation=270)
+    OverDown = auto(), "Pull the new image down from the top", partial(over, rotation=90)
+    OverLeft = auto(), "Pull the new image in from the right", partial(over, rotation=180)
+    OverRight = auto(), "Pull the new image in from the left", partial(over, rotation=0)
+    UncoverLeft = auto(), "Pull the old image away to the left", partial(unCover, rotation=0)
+    UncoverRight = auto(), "Pull the old image away to the right", partial(unCover, rotation=180)
+    UncoverUp = auto(), "Pull the old image away to the top", partial(unCover, rotation=90)
+    UncoverDown = auto(), "Pull the old image away to the bottom", partial(unCover, rotation=270)
+    WipeLeft = auto(), "Wipe left from the right side", partial(wipe, rotation=180)
+    WipeRight = auto(), "Pull the new image in from the left", partial(wipe, rotation=0)
+    WipeUp = auto(), "Wipe up from the bottom", partial(wipe, rotation=270)
+    WipeDown = auto(), "Wipe down from the top", partial(wipe, rotation=90)
     DownUp = auto(), "Push the old image down, and the new image up", downUp
-    SlideRightDown = auto(), "Slide the new image down from the top left corner", slideRightDown
-    SlideRightUp = auto(), "Slide the new image up from the bottom left corner", slideRightUp
-    SlideLeftDown = auto(), "Slide the new image down from the top left corner", slideLeftDown
-    SlideLeftUp = auto(), "Slide the new image up from the bottom left corner", slideLeftUp
-    ExpandRightDown = auto(), "Expand the image down from the top left corner", expandRightDown
-    ExpandRightUp = auto(), "Expand the image up from the bottom left corner", expandRightUp
-    ExpandLeftDown = auto(), "Expand the image down from the top right corner", expandLeftDown
-    ExpandLeftUp = auto(), "Expand the image up from the bottom right corner", expandLeftUp
-    ShrinkRightDown = auto(), "Shrink the image down from the top left corner", shrinkRightDown
-    ShrinkRightUp = auto(), "Shrink the image up from the bottom left corner", shrinkRightUp
-    ShrinkLeftDown = auto(), "Shrink the image down from the top right corner", shrinkLeftDown
-    ShrinkLeftUp = auto(), "Shrink the image up from the bottom right corner", shrinkLeftUp
-    CurtainHoriz = auto(), "Pull the new image in from the left and right", curtainHoriz
-    CurtainVert = auto(), "Pull the new image in from the op and bottom", curtainVert
-    CurtainOutHoriz = auto(), "Push the old image out to both sides", curtainOutHoriz
-    CurtainOutVert = auto(), "Push the old image out to the top and bottom", curtainOutVert
+    SlideRightDown = auto(), "Slide the new image down from the top left corner", partial(slide, rotation=0)
+    SlideRightUp = auto(), "Slide the new image up from the bottom left corner", partial(slide, rotation=270)
+    SlideLeftDown = auto(), "Slide the new image down from the top left corner", partial(slide, rotation=90)
+    SlideLeftUp = auto(), "Slide the new image up from the bottom left corner", partial(slide, rotation=180)
+    ExpandRightDown = auto(), "Expand the image down from the top left corner", partial(expand, rotation=0)
+    ExpandRightUp = auto(), "Expand the image up from the bottom left corner", partial(expand, rotation=270)
+    ExpandLeftDown = auto(), "Expand the image down from the top right corner", partial(expand, rotation=90)
+    ExpandLeftUp = auto(), "Expand the image up from the bottom right corner", partial(expand, rotation=180)
+    ShrinkRightDown = auto(), "Shrink the image down from the top left corner", partial(shrink, rotation=0)
+    ShrinkRightUp = auto(), "Shrink the image up from the bottom left corner", partial(shrink, rotation=270)
+    ShrinkLeftDown = auto(), "Shrink the image down from the top right corner", partial(shrink, rotation=90)
+    ShrinkLeftUp = auto(), "Shrink the image up from the bottom right corner", partial(shrink, rotation=180)
+    CurtainHoriz = auto(), "Pull the new image in from the left and right", partial(curtain, rotation=0)
+    CurtainVert = auto(), "Pull the new image in from the op and bottom", partial(curtain, rotation=90)
+    CurtainOutHoriz = auto(), "Push the old image out to both sides", partial(curtainOut, rotation=0)
+    CurtainOutVert = auto(), "Push the old image out to the top and bottom", partial(curtainOut, rotation=90)
     ZoomIn = auto(), "Expand the new in from the center", zoomIn
     ZoomOut = auto(), "Shrink the old image out to the center.", zoomOut
     ZoomOutIn = auto(), "Zoom the old image out, then the new image in", zoomOutIn
@@ -695,23 +567,23 @@ class TransitionTypes(StrEnum):
     SpinOut = auto(), "Spin and expand the new image in to the center", spinOut
     SpinIn = auto(), "Spin and shrink the old image out from the center", spinIn
     SpinInOut = auto(), "Spin and shrink the old image to the center, then spin and expand the new image in", spinInOut
-    Transporter = auto(), "Replace the old image pixel by pixel, randomly", transporter
-    Shimmer = auto(), "Replace pixels randomly, switching between old and new until the new image is complete", shimmer
+    Transporter = auto(), "Replace the old image pixel by pixel, randomly", partial(shimmer, keep=True)
+    Shimmer = auto(), "Replace pixels randomly, switching between old and new until the new image is complete", partial(shimmer, keep=False)
     LeanOut = auto(), "Lean the image out to the back", leanOut
     LeanIn = auto(),"Raise the image in from the back", leanIn
     LeanOutIn = auto(), "Lower the image out to the back, then raise the new image in from the back", leanOutIn
     FlipOut = auto(), "Lean the image out to the back", flipOut
     FlipIn = auto(),"Raise the image in from the back", flipIn
-    #FlipOutIn = auto(), "Lower the image out to the back, then raise the new image in from the back", flipOutIn
-    LeanFlip = auto(), "Yeah, whatever", leanflip
-    LeanFlipHoriz = auto(), "Yeah, whatever", leanfliphoriz
+    # FlipOutIn = auto(), "Lower the image out to the back, then raise the new image in from the back", flipOutIn
+    LeanFlip = auto(), "Lean the old image out at the bottom, and flip the new one in from the top", leanflip
+    LeanFlipHoriz = auto(), "Lean the old image out to the left, and flip the new one in from the right", leanfliphoriz
     # LowerFlip = auto(), "Complicated", lowerflip
-    PageTurn = auto(), "Page Turn", pageturn
+    #P ageTurn = auto(), "Page Turn", pageturn
     Boxes = auto(), "Replace boxes one at a time, top to bottom", boxes
     BoxesRandom = auto(), "Replace boxes one at a time, randomly", boxesrandom
     BoxesSnake = auto(), "Replace boxes one at a time, top to bottom, snaking", boxessnake
-    BarsHoriz = auto(), "Move bars of the image sideways", barshoriz
-    BarsVertical = auto(), "Move bars of the image vertically", barsvertical
+    BarsHoriz = auto(), "Move bars of the image sideways", partial(bars, rotation=0)
+    BarsVertical = auto(), "Move bars of the image vertically", partial(bars, rotation=90)
     # Make sure the Random transition goes last
     Random = auto(), "Pick a random transition", doRandom
 
@@ -723,10 +595,11 @@ def getTransition(name: str):
     return val.function
 
 if __name__ == "__main__":
-    import flaschen
-    import sys
     import random
+    import sys
     from pathlib import Path
+
+    import flaschen
 
     size = 64
 
