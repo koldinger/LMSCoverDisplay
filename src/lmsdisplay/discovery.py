c6cbc74c233986e3fda7e1e9f46890354046f00f
Eric Koldinger <kolding@washington.edu>
2026-07-11 15:11:32 -0700

    Code cleanup.   Add header.

--- src/lmsdisplay/discovery.py
+++ src/lmsdisplay/discovery.py
@@ -1,3 +1,32 @@
+# vim: set et sw=4 sts=4 fileencoding=utf-8:
+#
+# Copyright 2025-2026, Eric Koldinger, All Rights Reserved.
+# kolding@washington.edu
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the copyright holder nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 import socket
 
 from collections import namedtuple
@@ -13,19 +42,17 @@ LmsInstance = namedtuple("LmsInstance", [ATTR_HOST, ATTR_PORT])
 
 def discover_lms(timeout=DEFAULT_DISCOVERY_TIMEOUT):
     """ Scan network for Logitech Media Servers. """
-    lms_ip = "<broadcast>"
-    lms_port = DISCOVERY_PORT
     lms_msg = b"eJSON\0"
 
     entries = set()
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     sock.settimeout(timeout)
     sock.bind(("", 0))
 
     try:
-        sock.sendto(lms_msg, (lms_ip, lms_port))
+        sock.sendto(lms_msg, ("<broadcast>", DISCOVERY_PORT))
 
         while True:
             try:
