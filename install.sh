c0660ce07eda6ffc476eba9177bf9bdb73533d51
Eric Koldinger <kolding@washington.edu>
2026-07-27 14:16:02 -0700

    Updated to load the needed module for i2c.

--- install.sh
+++ install.sh
@@ -5,12 +5,17 @@
 
 # Disable sound
 sed -i s/dtparam=audio=on/dtparam=audio=off/ /boot/firmware/config.txt
+sed -i s/#dtparam=i2c_arm/dtparam=i2c_arm/ /boot/firmware/config.txt
 
 # Enable isolcpus=3
 sed -i "s/$/ isolcpus=3/" /boot/firmware/cmdline.txt
 
 cat << EOF >> /etc/modprobe.d/blacklist-custom.conf
 blacklist snd_bcm2835
+EOF
+
+cat << EOF >> /etc/modules-load.d/i2c_dev.conf
+i2c_dev
 EOF
 
 # Import important packages
