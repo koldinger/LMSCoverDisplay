f8dff13f438ab2c8d37d386284ae8514fd8ee29f
Eric Koldinger <kolding@washington.edu>
2026-08-17 15:23:32 -0700

    Get closer.   Still needs work.

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sed -i s/dtparam=audio=on/dtparam=audio=off/ /boot/firmware/config.txt
 sed -i s/#dtparam=i2c_arm/dtparam=i2c_arm/ /boot/firmware/config.txt
 
 # Enable isolcpus=3
-sed -i "s/$/ isolcpus=3/" /boot/firmware/cmdline.txt
+sed -i "s/$/ isolcpus=domain,managed_irq,3 nohz_full=3 rcu_nocbs=3 irqaffinity=0,1,2/" /boot/firmware/cmdline.txt
 
 cat << EOF >> /etc/modprobe.d/blacklist-custom.conf
 blacklist snd_bcm2835
@@ -21,7 +21,7 @@ EOF
 # Import important packages
 apt update -y
 apt upgrade -y
-apt install -git i2c-tools python3-pip python3-setuptools procs btop fd-find bat -y
+apt install -y git i2c-tools python3-pip python3-setuptools procs btop fd-find bat
 
 # Disable Externally Managed Python sntuff
 rm /usr/lib/python3*/EXTERNALLY-MANAGED
@@ -29,16 +29,19 @@ rm /usr/lib/python3*/EXTERNALLY-MANAGED
 # Install uv
 pip install uv 
 
-uv pip install --system git+https://github.com/hzeller/rpi-rgb-led-matrix.git
-
 # Start installing files
-cp ft-server /usr/local/bin
+#cp ft-server /usr/local/bin
 
 #uv pip install --system LMSTools
+
+#uv pip install --system git+https://github.com/hzeller/rpi-rgb-led-matrix.git
 #uv pip install --system .
 
+uv pip install --system git+https://github.com/koldinger/WifiSelect
+#uv pip install --system git+https://github.com/koldinger/WifiSelect
+
 # Install service files
-#SERVICES=("ft-server.service" "lmsconfig.service" "lmsdisplay.service" "lmsremote.service")
-SERVICES=("lmsconfig.service" "lmsdisplay.service" "lmsremote.service")
+#SERVICES=("ft-server.service" "lmsconfig.service" "lmsdisplay.service" "lmsremote.service" "wifiselect.service")
+SERVICES=("ft-server.service lmsconfig.service" "lmsdisplay.service" "lmsremote.service" "wifiselect.service")
 cp "${SERVICES[@]}" /usr/lib/systemd/system
 systemctl enable "${SERVICES[@]}"
